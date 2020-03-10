7838be296cca863de23a0d4e1d5387b2745ac863
Topher Pedersen <chris@topherpedersen.com>
2020-03-10 11:24:04 -0500

    made first successful api call

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -14,7 +6,14 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
+
+// The MovieDB JavaScript Libraries Recommended by The MovieDB
+// https://github.com/EtienneWan/tmdb-js/
+// https://github.com/cavestri/themoviedb-javascript-library/
+
+// Discover Endpoint Reference: https://www.themoviedb.org/documentation/api/discover
 
 import { 
   API_KEY, 
@@ -26,12 +25,33 @@ class App extends React.Component {
     super(props)
   }
 
+  fetchMovieData() {
+
+    // Construct our MovieDB Endpoint URL and Add HTTP GET Request Parameters
+    let URL = 'https://api.themoviedb.org/3/discover/movie';
+    URL = URL + '?api_key=' + API_KEY;
+    URL = URL + '&sort_by=popularity.desc'; 
+
+    fetch(URL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const dataStr = JSON.stringify(data);
+        alert(dataStr)
+      });
+  }
+
   render() {
     return(
       <SafeAreaView>
+
         <Text>7-Eleven Code Test</Text>
-        <Text>{API_KEY}</Text>
-        <Text>{ACCESS_TOKEN}</Text>
+
+        <Button
+          title="Fetch Movie Data"
+          onPress={ () => this.fetchMovieData() }/>
+
       </SafeAreaView>
     );
   }
