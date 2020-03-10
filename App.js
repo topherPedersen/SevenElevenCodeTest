5606f8849d91765733d6441c4d12ca53d15ebcc1
Topher Pedersen <chris@topherpedersen.com>
2020-03-10 11:51:43 -0500

    moved components to their own components directory

--- App.js
+++ App.js
@@ -9,58 +9,18 @@ import {
   Button,
 } from 'react-native';
 
-// The MovieDB JavaScript Libraries Recommended by The MovieDB
-// https://github.com/EtienneWan/tmdb-js/
-// https://github.com/cavestri/themoviedb-javascript-library/
-
-// Discover Endpoint Reference: https://www.themoviedb.org/documentation/api/discover
-
-import { 
-  API_KEY, 
-  ACCESS_TOKEN 
-} from './apikey.js';
+import TopMovies from './components/TopMovies';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
   }
 
-  fetchMovieData() {
-
-    let topMovies;
-
-    // Construct our MovieDB Endpoint URL and Add HTTP GET Request Parameters
-    let URL = 'https://api.themoviedb.org/3/discover/movie';
-    URL = URL + '?api_key=' + API_KEY;
-    URL = URL + '&sort_by=popularity.desc'; 
-
-    fetch(URL)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-
-        // const dataStr = JSON.stringify(data);
-        // alert(dataStr)
-        topMovies = data.results.map( (result) => { 
-          return result.original_title;
-        });
-
-        alert(topMovies[0]);
-
-      });
-      // .then(alert(topMovies[0]));
-  }
-
   render() {
     return(
       <SafeAreaView>
 
-        <Text>7-Eleven Code Test</Text>
-
-        <Button
-          title="Fetch Movie Data"
-          onPress={ () => this.fetchMovieData() }/>
+        <TopMovies/>
 
       </SafeAreaView>
     );
---
+++ components/TopMovies.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+// The MovieDB JavaScript Libraries Recommended by The MovieDB
+// https://github.com/EtienneWan/tmdb-js/
+// https://github.com/cavestri/themoviedb-javascript-library/
+
+// Discover Endpoint Reference: https://www.themoviedb.org/documentation/api/discover
+
+import { 
+  API_KEY, 
+  ACCESS_TOKEN 
+} from '../apikey.js';
+
+class TopMovies extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  fetchMovieData() {
+
+    let topMovies;
+
+    // Construct our MovieDB Endpoint URL and Add HTTP GET Request Parameters
+    let URL = 'https://api.themoviedb.org/3/discover/movie';
+    URL = URL + '?api_key=' + API_KEY;
+    URL = URL + '&sort_by=popularity.desc'; 
+
+    fetch(URL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+
+        // const dataStr = JSON.stringify(data);
+        // alert(dataStr)
+        topMovies = data.results.map( (result) => { 
+          return result.original_title;
+        });
+
+        alert(topMovies[0]);
+
+      });
+      // .then(alert(topMovies[0]));
+  }
+
+  render() {
+    return(
+      <View>
+
+        <Text>Top Movies...</Text>
+
+        <Button
+          title="Fetch Movie Data"
+          onPress={ () => this.fetchMovieData() }/>
+
+      </View>
+    );
+  }
+}
+
+export default TopMovies;
